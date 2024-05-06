8962f17bba56a81eae512c286e30766221c29e53
EquinoxEgo <153934087+EquinoxEgo@users.noreply.github.com>
2024-05-06 08:19:43 -0400

    Update Final.js

--- JAVA/Final.js
+++ JAVA/Final.js
@@ -1,4 +1,3 @@
-//HOTEL JAVASCRIPT****************************************************************/
 let slideIndex = 0;
 showSlides();
 
@@ -20,31 +19,35 @@ function showSlides() {
 }
 
 
-// Swiper Cards JS Start
-let swiperCards = new Swiper('.card_content', {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
+// Select tab content item
+function selectItem(e) {
+ // Remove all show and border classes
+ removeBorder();
+ removeShow();
+ // Add border to current tab item
+ this.classList.add('tab-border');
+ // Grab content item from DOM
+ const tabContentItem = document.querySelector(`#${this.id}-content`);
+ // Add show class
+ tabContentItem.classList.add('show');
+}
+function removeBorder() {
+ tabItems.forEach(item => {
+  item.classList.remove('tab-border');
+ });
+}
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+// Remove show class from all content items
+function removeShow() {
+ tabContentItems.forEach(item => {
+  item.classList.remove('show');
+ });
+}
 
-
-// Changes the amount of cards on the page depending on the size of the webpage
-  breakpoints: {
-    600: {
-      slidesPerView: 2,
-    },
-    968: {
-      slidesPerView: 3,
-    }
-  },
+// Listen for tab item click
+tabItems.forEach(item => {
+ item.addEventListener('click', selectItem);
 });
-
-
-// Swiper Cards JS End
-
