9b4463df625131418a8213e07ce372e4bfc29888
EquinoxEgo <goodenaidien@Gmail.com>
2024-05-06 10:41:10 -0400

    .

--- JAVA/Final.js
+++ JAVA/Final.js
@@ -51,3 +51,13 @@ function removeShow() {
 tabItems.forEach(item => {
  item.addEventListener('click', selectItem);
 });
+
+
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
