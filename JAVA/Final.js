04dff3ef207a2ce7069490d7539d1cdfd769887d
EquinoxEgo <goodenaidien@Gmail.com>
2024-05-09 10:41:46 -0400

    setback

--- JAVA/Final.js
+++ JAVA/Final.js
@@ -1,4 +1,3 @@
-import data from "../JAVA/Index.Json" assert { type: "json" }
 
 let slideIndex = 0;
 showSlides();
@@ -8,15 +7,15 @@ function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
@@ -26,32 +25,32 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
- // Remove all show and border classes
- removeBorder();
- removeShow();
- // Add border to current tab item
- this.classList.add('tab-border');
- // Grab content item from DOM
- const tabContentItem = document.querySelector(`#${this.id}-content`);
- // Add show class
- tabContentItem.classList.add('show');
+  // Remove all show and border classes
+  removeBorder();
+  removeShow();
+  // Add border to current tab item
+  this.classList.add('tab-border');
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add('show');
 }
 function removeBorder() {
- tabItems.forEach(item => {
-  item.classList.remove('tab-border');
- });
+  tabItems.forEach(item => {
+    item.classList.remove('tab-border');
+  });
 }
 
 // Remove show class from all content items
 function removeShow() {
- tabContentItems.forEach(item => {
-  item.classList.remove('show');
- });
+  tabContentItems.forEach(item => {
+    item.classList.remove('show');
+  });
 }
 
 // Listen for tab item click
 tabItems.forEach(item => {
- item.addEventListener('click', selectItem);
+  item.addEventListener('click', selectItem);
 });
 
 
@@ -63,3 +62,28 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  let menuList = document.getElementById("menuList")
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "300px";
+  }
+  else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+window.addEventListener('click', toggleMenu);
